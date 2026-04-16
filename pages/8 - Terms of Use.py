10e1edeae02ac71bdf492812e20459a5fca9de66
r <146114263+xngxpta@users.noreply.github.com>
2026-04-16 22:54:06 +0530

    Update 8 - Terms of Use.py

--- pages/8 - Terms of Use.py
+++ pages/8 - Terms of Use.py
@@ -13,16 +13,17 @@ direc = os.getcwd()
 #         return Path(file_path).read_text()
 #     except FileNotFoundError:
 #         return f"Error: The file '{file_path}' was not found."
+url_stripe = "https://buy.stripe.com/9B6bJ3gWS87G97b80q0480f"
 url_stripe_2 = "https://buy.stripe.com/6oUbJ35eaew4bfj0xY0480e"
 # Specify the path to your markdown file
 file0 = f'{direc}/pages/appdata/Terms-of-Use.pdf' # Replace with your file name/path
 st.sidebar.link_button("Join Us/day!", url_stripe, type="primary",
-               disabled=False, use_container_width="True")
+                        disabled=False, use_container_width="True")
+st.sidebar.link_button("Become a Patron!", url_stripe_2, type="primary",
+                       disabled=False, use_container_width="True")
 with open(file0, "rb") as f:
-    
-    base64_pdf = base64.b64encode(f.read()).decode('utf-8')
-    
-pdf_display0 = f'<embed src="data:application/pdf;base64,{base64_pdf}" width="350" height="250" type="application/pdf">'
+        base64_pdf = base64.b64encode(f.read()).decode('utf-8')
+    pdf_display0 = f'<embed src="data:application/pdf;base64,{base64_pdf}" width="350" height="250" type="application/pdf">'
 st.markdown(pdf_display0, unsafe_allow_html=True)
 # Read the file content
 #intro_markdown = read_markdown_file(markdown_file_path)
