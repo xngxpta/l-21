6750c774596b9df46caaa69b201b1b7d6615b5ec
r <146114263+xngxpta@users.noreply.github.com>
2026-04-18 22:22:19 -0400

    Update sidebar button label for Stripe access

--- Home.py
+++ Home.py
@@ -39,7 +39,7 @@ url_blog = 'https://www.alphaledgr.com/Blog'
 url_linkedin = "https://www.linkedin.com/company/ledgrapp/"
 url_stripe = "https://book.stripe.com/9B6bJ3gWS87G97b80q0480f"
 url_stripe_2 = "https://buy.stripe.com/6oUbJ35eaew4bfj0xY0480e"
-st.sidebar.link_button("Acces Ledgr/ Day!", url_stripe, type="primary",
+st.sidebar.link_button("Access Pro / Day!", url_stripe, type="primary",
                    disabled=False, use_container_width="True")
 st.sidebar.link_button("Become a Patron!", url_stripe_2, type="primary",
                     disabled=False, use_container_width="True")
